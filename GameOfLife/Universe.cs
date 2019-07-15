e9fba0ef0a8ffb4c7fb51e6e69eeaee01af11f55
Pedro Santos <pedro.santos.personal@gmail.com>
2019-07-16 00:55:53 +0200

    Rename spots to seed

--- GameOfLife/Universe.cs
+++ GameOfLife/Universe.cs
@@ -6,36 +6,36 @@ namespace GameOfLife
 {
     public class Universe
     {
-        private readonly IDictionary<Position, Cell> _spots;
+        private readonly IDictionary<Position, Cell> _seed;
 
-        public Universe(IDictionary<Position, Cell> spots)
+        public Universe(IDictionary<Position, Cell> seed)
         {
-            _spots = spots;
+            _seed = seed;
         }
 
         public Universe Tick()
         {
-            var spots = new Dictionary<Position, Cell>();
+            var newSeed = new Dictionary<Position, Cell>();
 
-            foreach (var (position, cell) in _spots)
+            foreach (var (position, cell) in _seed)
             {
-                spots[position] = cell.Tick(NeighboursTo(position));
+                newSeed[position] = cell.Tick(NeighboursTo(position));
             }
             
-            return new Universe(spots);
+            return new Universe(newSeed);
         }
 
         private Neighbours NeighboursTo(Position position)
         {
             return new Neighbours()
-                .Add(_spots.GetValueOrNone(position.Above()))
-                .Add(_spots.GetValueOrNone(position.Bellow()))
-                .Add(_spots.GetValueOrNone(position.Left()))
-                .Add(_spots.GetValueOrNone(position.Right()))
-                .Add(_spots.GetValueOrNone(position.AboveLeft()))
-                .Add(_spots.GetValueOrNone(position.AboveRight()))
-                .Add(_spots.GetValueOrNone(position.BellowLeft()))
-                .Add(_spots.GetValueOrNone(position.BellowRight()));
+                .Add(_seed.GetValueOrNone(position.Above()))
+                .Add(_seed.GetValueOrNone(position.Bellow()))
+                .Add(_seed.GetValueOrNone(position.Left()))
+                .Add(_seed.GetValueOrNone(position.Right()))
+                .Add(_seed.GetValueOrNone(position.AboveLeft()))
+                .Add(_seed.GetValueOrNone(position.AboveRight()))
+                .Add(_seed.GetValueOrNone(position.BellowLeft()))
+                .Add(_seed.GetValueOrNone(position.BellowRight()));
         }
 
         public override bool Equals(object obj)
@@ -43,13 +43,13 @@ namespace GameOfLife
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
             return obj.GetType() == GetType() 
-                   && _spots.Count() == ((Universe)obj)._spots.Count 
-                   && !_spots.Except(((Universe)obj)._spots).Any();
+                   && _seed.Count() == ((Universe)obj)._seed.Count 
+                   && !_seed.Except(((Universe)obj)._seed).Any();
         }
 
         public override int GetHashCode()
         {
-            return (_spots != null ? _spots.GetHashCode() : 0);
+            return (_seed != null ? _seed.GetHashCode() : 0);
         }
 
         public static bool operator ==(Universe left, Universe right)
