934e1ad0794185c93f93c45f1c01f2ca9911cd00
Pedro Santos <pedro.santos.personal@gmail.com>
2019-07-15 23:59:02 +0200

    Inline variables in Position tests@

--- GameOfLifeTests/PositionShould.cs
+++ GameOfLifeTests/PositionShould.cs
@@ -16,65 +16,49 @@ namespace GameOfLifeTests
         [Fact]
         public void CalculatePositionToTheLeft()
         {
-            var newPosition = _position.Left();
-
-            newPosition.Should().Be(new Position(2, 3));
+            _position.Left().Should().Be(new Position(2, 3));
         }
         
         [Fact]
         public void CalculatePositionToTheRight()
         {
-            var newPosition = _position.Right();
-
-            newPosition.Should().Be(new Position(4, 3));
+            _position.Right().Should().Be(new Position(4, 3));
         }
         
         [Fact]
         public void CalculatePositionAbove()
         {
-            var newPosition = _position.Above();
-
-            newPosition.Should().Be(new Position(3, 2));
+            _position.Above().Should().Be(new Position(3, 2));
         }
         
         [Fact]
         public void CalculatePositionAboveLeft()
         {
-            var newPosition = _position.AboveLeft();
-
-            newPosition.Should().Be(new Position(2, 2));
+            _position.AboveLeft().Should().Be(new Position(2, 2));
         }
         
         [Fact]
         public void CalculatePositionAboveRight()
         {
-            var newPosition = _position.AboveRight();
-
-            newPosition.Should().Be(new Position(4, 2));
+            _position.AboveRight().Should().Be(new Position(4, 2));
         }
         
         [Fact]
         public void CalculatePositionBellow()
         {
-            var newPosition = _position.Bellow();
-
-            newPosition.Should().Be(new Position(3, 4));
+            _position.Bellow().Should().Be(new Position(3, 4));
         }
         
         [Fact]
         public void CalculatePositionBellowLeft()
         {
-            var newPosition = _position.BellowLeft();
-
-            newPosition.Should().Be(new Position(2, 4));
+            _position.BellowLeft().Should().Be(new Position(2, 4));
         }
         
         [Fact]
         public void CalculatePositionBellowRight()
         {
-            var newPosition = _position.BellowRight();
-
-            newPosition.Should().Be(new Position(4, 4));
+            _position.BellowRight().Should().Be(new Position(4, 4));
         }
     }
 }
